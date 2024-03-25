1dbda740ad00763af0e2c932d3126866c1d49d01
Grubneac <grubneac@yandex.ru>
2024-03-25 19:30:12 +0200

    hw7

---
+++ src/lesson7/hw/ArrayContest.java
@@ -0,0 +1,29 @@
+package lesson7.hw;
+
+
+public class ArrayContest {
+    // 1. Дан массив целых чисел. Вернуть массив только четных чисел.
+    public static int[] filterIntArray(int[] array) {
+        return null;
+    }
+
+    // 2. Дан массив строк. Вернуть массив строк содержащих букву a.
+    public static String[] filterStringArray(String[] array) {
+        return null;
+    }
+
+    // 3. Дан массив целых чисел. Вернуть массив квадратов этих чисел.
+    public static int[] powerIntArray(int[] array) {
+        return null;
+    }
+
+    // 4. Дан массив строк. Проверить содержится ли слово в массиве, игнорируя case.
+    public static boolean checkWord(String[] array, String word) {
+        return true;
+    }
+
+    // 5. Дана матрица целых чисел. Вернуть строку в матрице с наибольшей суммой элементов.
+    public static int[] biggestRowInMatrix(int[][] matrix) {
+        return null;
+    }
+}
