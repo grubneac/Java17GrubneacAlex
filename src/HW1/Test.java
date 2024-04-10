6e4e2a9200470791f95fe1f32e4576fccbeacf8f
Grubneac <grubneac@yandex.ru>
2024-04-10 19:15:27 +0300

    HW1 commit 2

--- src/HW1/Test.java
+++ src/HW1/Test.java
@@ -3,6 +3,7 @@ package HW1;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Branch HW1");
+        System.out.println("Branch HW1 ver 2");
     }
 
 }
